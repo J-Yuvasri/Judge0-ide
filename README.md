da23e5928f630706e42b772fe50cfadf9794b3e6
Yuvasri <yuvasri.ec17@bitsathy.ac.in>
2020-07-12 21:16:33 -0400

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-### Online IDE 
-<hr />
-<img src="images/first.png" width="250" height="250">
-
-<hr />
-## About
-This is a clone to the Website https://ide.judge0.com/. It is built using the api from the RapidApi named 
-https://rapidapi.com/hermanzdosilovic/api/judge0. This Website is buikt using reactjs hosted in github pages. 
---
+++ precache-manifest.61b3655c11d1be0fe0612945635df022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c92d69c995bd264070f2d6d2200d973",
+    "url": "/Judge0-ide/index.html"
+  },
+  {
+    "revision": "334ab016174e618f20a2",
+    "url": "/Judge0-ide/static/css/main.4e9684e2.chunk.css"
+  },
+  {
+    "revision": "0edddb5c2684cffdafe2",
+    "url": "/Judge0-ide/static/js/2.bee56b98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Judge0-ide/static/js/2.bee56b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334ab016174e618f20a2",
+    "url": "/Judge0-ide/static/js/main.dcd1f808.chunk.js"
+  },
+  {
+    "revision": "170dbd3c5f4e4bf4f781",
+    "url": "/Judge0-ide/static/js/runtime-main.674cca47.js"
+  }
+]);
